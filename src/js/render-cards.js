10ddbdb103bbcd7e13f569b3fecb3698038170fd
Olexandr Tkachuk <temishena@gmail.com>
2023-01-27 22:41:35 +0200

    розмітка + стилі library-section

--- src/js/render-cards.js
+++ src/js/render-cards.js
@@ -1,28 +1,37 @@
-import { getTrendMovies, getGenresMovies } from "./api-fetch"
+import { getTrendMovies, getGenresMovies } from './api-fetch';
 
 // ;
 let genreList = {};
 
 const mainGallery = document.querySelector('.mainGallery');
 const galleryList = document.querySelector('.movieList');
+
 (function () {
-    getTrendMovies().then(movies => createMarkupOfTrendingMovies(movies)).catch(err => console.log(err));
+  getTrendMovies()
+    .then(movies => createMarkupOfTrendingMovies(movies))
+    .catch(err => console.log(err));
 })();
 function getGenresNames() {
-    getGenresMovies().then(genres => genres.forEach(genre => (genreList[genre['id']] = genre['name']))).catch(err => console.log(err));
+  getGenresMovies()
+    .then(genres =>
+      genres.forEach(genre => (genreList[genre['id']] = genre['name']))
+    )
+    .catch(err => console.log(err));
 }
 
 function createMarkupOfTrendingMovies(obj) {
-    if (obj.results.length) {
-        const markup = obj.results.map(({
-            id,
-            title,
-            name,
-            poster_path,
-            genre_ids,
-            release_date,
-            first_air_date,
-            vote_average,
+  if (obj.results.length) {
+    const markup = obj.results
+      .map(
+        ({
+          id,
+          title,
+          name,
+          poster_path,
+          genre_ids,
+          release_date,
+          first_air_date,
+          vote_average,
         }) => `<li class="movieCard" data="${id}">
       <div class="movieCard__img-wrapper">
       <img src="https://image.tmdb.org/t/p/w500/${poster_path}"
@@ -34,14 +43,14 @@ function createMarkupOfTrendingMovies(obj) {
       <div class="movieCard__text">
         <h2 class="movieCard__title">${(title || name).toUpperCase()}</h2>
         <p class="movieCard__info"> ${genre_ids}| ${new Date(
-            release_date || first_air_date
+          release_date || first_air_date
         ).getFullYear()}
           <span class="movieCard__rate">${vote_average.toFixed(1)}</span></p>
       </div>
       </li>
 `
-        )
-            .join('');
-        galleryList.insertAdjacentHTML('afterBegin', markup);
-    }
+      )
+      .join('');
+    galleryList.insertAdjacentHTML('afterBegin', markup);
+  }
 }
