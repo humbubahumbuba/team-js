93020d4f07bc0cfdbf8efc5a3d34acd7d284cd96
mvxseeds <me.semena@gmail.com>
2023-01-27 15:12:56 +0200

    localStorage + content loading
    Added localStorage get/set functions
    Added page content loader

---
+++ src/js/page-content-loader.js
@@ -0,0 +1,72 @@
+// *** CURRENT PAGE CONTENT STORE & RENDER *** //
+// uses const CRT_USER = 'current-user';
+// current user with id: 0 - named temp
+// import also has queue/watched, when logged in - import's q/w cleared, data moved to users q/w
+// also need to check if q/w has movies from temp
+
+// import movie content fetch fn
+import { getTrendMovies, getGenresMovies } from "./api-fetch";
+// import localStorage getter & setter
+import { getStorageData, setStorageData } from "./ls-data";
+
+import { lskeys } from "./ls-data";
+const { HOME_CONTENT, GENRES, CRT_CONTENT, CRT_USER } = lskeys;
+
+// load genres once
+// const genres = await getGenresMovies();
+
+
+// load home page content once
+export async function loadPageContent(lsKey=HOME_CONTENT) {
+    if (lsKey === HOME_CONTENT) {
+        const response = await getTrendMovies();
+        setStorageData(lsKey, response);
+        return response;
+    }
+    if (lsKey === GENRES) {
+        const response = await getGenresMovies();
+        setStorageData(lsKey, response);
+    }
+}
+
+
+/* load content on page load
+if (!pageContent) {
+    loadPageContent();
+}
+*/
+
+
+
+/*
+// load current page content once
+// do not load if curent page = home page
+if (page === homePage) {
+	loadPageContent(pageContent);
+} else {
+	setStorageData(CRT_CONTENT, apiRequest(query));
+	pageContent = getStorageData(CRT_CONTENT);
+	loadPageContent(pageContent);
+}
+
+
+// check if any user is currently logged in
+const currentUser = getStorageData(CRT_USER);
+
+// if no user logged in - use temporary queue and watched
+if (!currentUser) {
+	setStorageData(TMP_QUEUE, []);
+	setStorageData(TMP_WATCHED, []);
+}
+
+
+// functions for adding to queue/watched
+
+
+// function for importing temporary queue/watched to logged in user
+
+
+
+// *** //
+
+*/
