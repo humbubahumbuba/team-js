0b1c8cdbd0b4d882d9c0a86479e70b60238fd6e0
Viktor Oliinyk <kanoute1992@gmail.com>
2023-01-28 21:32:05 +0200

    add some code 2

--- src/js/api-fetch.js
+++ src/js/api-fetch.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
+import { onSpinnerDisabled, onSpinnerEnabled } from './loader-spinner';
 
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'e8d94f3e976148bda0a5c640d4df112b';
 
 export async function getTrendMovies() {
     try {
+        onSpinnerEnabled();
         const response = await axios.get(`${BASE_URL}trending/all/day?api_key=${API_KEY}`);
-        // console.log(response.data)
+        onSpinnerDisabled();
         return response.data;
 
     } 
@@ -17,7 +19,9 @@ export async function getTrendMovies() {
 
 export async function getGenresMovies() {
     try {
+      onSpinnerEnabled();
         const response = await axios.get(`${BASE_URL}genre/movie/list?api_key=${API_KEY}&language=en-US`);
+        onSpinnerDisabled();
         // console.log(response.data.genres)
         return response.data.genres;
     }
