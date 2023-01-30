eb048e9b4f4e603ac5fce106272f7692b5768c3f
Nataliia-Bilets <natalia_bilets@ukr.net>
2023-01-30 10:43:44 +0200

    feature/darkTheme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './js/api-fetch';
 import './js/render-cards';
 import './js/modalMovie.js';
+import './js/darktheme';
 
 import './js/ls-data';
 import './js/page-content-loader';
 import './js/auth';
 import './js/search-movies';
 import './js/loader-spinner';
+
