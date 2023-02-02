09638e5b86efaff5a9d17722008026dd12d7c3ac
SashaIlchenko <sandra.ilchenko11@gmail.com>
2023-02-02 19:44:00 +0200

    bug fixed

--- src/js/modalMovie.js
+++ src/js/modalMovie.js
@@ -5,6 +5,8 @@ import { textModalBtn, addWatchList, addQueueList } from './watchQueueBtn';
 export const backdgop = document.querySelector('.js-backdrop');
 const modalMovie = document.querySelector('.js-modal-movie');
 const movieBox = document.querySelector('.js-movie-box');
+const movieImg = document.querySelector('.library-empty');
+const moviePoster = document.querySelector('.library-empty__image');
 export const closeModalBtn = document.querySelector('.js-close-btn');
 const movieList =
   document.querySelector('.js-movieList') ||
@@ -41,12 +43,18 @@ function closeModalOnEscapePress(event) {
   if (event.code === 'Escape') {
     hideModal();
     document.removeEventListener('keydown', closeModalOnEscapePress);
+    movieImg.classList.add('visually-hidden');
+    moviePoster.classList.add('visually-hidden');
+
   }
 }
 
 function onBackdropClick(event) {
   if (event.target === event.currentTarget) {
     hideModal();
+    movieImg.classList.add('visually-hidden');
+    moviePoster.classList.add('visually-hidden');
+
   }
 }
 
@@ -57,6 +65,7 @@ function showModal() {
 }
 
 function hideModal() {
+
   modalMovie.parentElement.classList.add('is-hidden');
   modalMovie.classList.add('is-hidden');
   document.body.classList.remove('no-scroll');
@@ -87,23 +96,22 @@ function makeMovieMarkup(data) {
   genres.forEach(genre => movieGenres.push(genre.name));
   const imageBaseUrl = 'https://image.tmdb.org/t/p/w500/';
   const markup = `
-        <img class="modal-movie__poster" src="${imageBaseUrl}/${poster_path}" alt="${
-    title || name || original_name
-  }" loading="lazy" >
+        <img class="modal-movie__poster" src="${imageBaseUrl}/${poster_path}" alt="${title || name || original_name
+    }" loading="lazy" >
             <div class="movie-card">
                 <h2 class="movie-card__title">${(
-                  title ||
-                  name ||
-                  original_name
-                ).toUpperCase()}</h2>
+      title ||
+      name ||
+      original_name
+    ).toUpperCase()}</h2>
                 <table class="info-table">
                     <tbody>
                         <tr>
                             <td class="info-table__classification">Vote / Votes</td>
                             <td class="info-table__classification-data">
                                 <span class="rating rating--vote">${vote_average.toFixed(
-                                  1
-                                )}</span>
+      1
+    )}</span>
                                 <span class="rating--slash-color">/</span>
                                 <span class="rating rating--votes">${vote_count}</span>
                             </td>
@@ -114,15 +122,13 @@ function makeMovieMarkup(data) {
                         </tr>
                         <tr>
                             <td class="info-table__classification">Original Title</td>
-                            <td class="info-table__classification-data">${
-                              title || original_name
-                            }</td>
+                            <td class="info-table__classification-data">${title || original_name
+    }</td>
                         </tr>
                         <tr>
                             <td class="info-table__classification info-table__classification--not-margin">Genres</td>
-                            <td class="info-table__classification-data">${
-                              movieGenres.join(', ') || 'No information..'
-                            }</td>
+                            <td class="info-table__classification-data">${movieGenres.join(', ') || 'No information..'
+    }</td>
                         </tr>
                     </tbody>
                 </table>
